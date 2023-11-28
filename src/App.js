e9c9cd744c8f31a28ec7ccacbf550ba141c9a4b5
mohammed shoaib <shoaibme76@gmail.com>
2023-11-28 17:28:16 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+  const [recipientList, setRecipientList] = useState('');
+
+  const sendEmails = async () => {
+    // Send the data to the backend for processing (will be implemented in Node.js)
+    const response = await fetch('http://localhost:3001/send-emails', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ subject, message, recipientList }),
+    });
+
+    if (response.ok) {
+      console.log('Emails sent successfully!');
+    } else {
+      console.error('Error sending emails');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bulk Email Tool</h1>
+      <label>
+        Subject:
+        <input type="text" value={subject} onChange={(e) => setSubject(e.target.value)} />
+      </label>
+      <label>
+        Message:
+        <textarea value={message} onChange={(e) => setMessage(e.target.value)} />
+      </label>
+      <label>
+        Recipient List (comma-separated email addresses):
+        <textarea value={recipientList} onChange={(e) => setRecipientList(e.target.value)} />
+      </label>
+      <button onClick={sendEmails}>Send Emails</button>
     </div>
   );
 }
